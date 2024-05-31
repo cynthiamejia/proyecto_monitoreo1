4d19142db9f4c82a6895cfc7ea1cc2de3858e507
Cynthia Mejia <cynthia1499@hotmail.com>
2024-05-31 14:24:01 -0600

    correcciones

--- prog01.py
+++ prog01.py
@@ -10,7 +10,7 @@ def cargar_y_limpiar(url, drop_cols, rename_cols):
     df['created_at'] = pd.to_datetime(df['created_at'])  
     return df
 
-ch4co8_url = "https://api.thingspeak.com/channels/2378137/feeds.csv?start=2024-02-08offset=UTC-06:00&end=2024-02-12offset=UTC-06:00"
+ch4co8_url = "https://api.thingspeak.com/channels/2378137/feeds.csv?start=2024-04-30offset=UTC-06:00&end=2024-05-04offset=UTC-06:00"
 temphum8_url = "https://api.thingspeak.com/channels/1935500/feeds.csv?start=2024-05-27offset=UTC-06:00&end=2024-05-30offset=UTC-06:00"
 ch4co8 = cargar_y_limpiar(ch4co8_url, ['entry_id', 'field1', 'field4', 'field5', 'field6'], {'field2': 'Metano', 'field3': 'Monoxido'})
 temphum8 = cargar_y_limpiar(temphum8_url, ['entry_id', 'field4', 'field5', 'field6'], {'field1': 'Humedad', 'field2': 'Temperatura', 'field3': 'CO2'})
